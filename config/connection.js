760aa17518232480623b589aeafddb7612b04970
Trie2168 <trie0037@gmail.com>
2018-08-13 18:21:40 -0500

    HerokuAdd

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "Vint@ge0916!",
-    database: "burger_db"
-});
+var connection;
 
-connection.connect(function(err){
-    if (err) {
-        console.error("Error connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as id " + connection.threadId);
-}
-);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Vint@ge0916!",
+        database: "burger_db"
+    });
+};
+
+connection.connect
 
 module.exports = connection;
