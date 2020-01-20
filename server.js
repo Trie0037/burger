f818962508c1caf66a4afa5684bf123e91c9af6d
Trie0037 <trie0037@gmail.com>
2020-01-19 22:22:58 -0600

    ping

--- server.js
+++ server.js
@@ -21,3 +21,9 @@ app.use(routes);
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
+var https = require("https");
+setInterval(function() {
+    https.get("https://eatdaburger187.herokuapp.com/");
+}, 300000); // ping app every 5 minutes (300000)
+
